1abded360b488ac98e17446848339d32f2234af6
Sunanda123-png <sunanda02@gmail.com>
2021-09-23 09:40:17 +0530

    checking player can play or not

--- src/com/sunanda/Tictoe.java
+++ src/com/sunanda/Tictoe.java
@@ -1,7 +1,7 @@
 package com.sunanda;
 import java.util.Scanner;
 
-import java.util.Scanner;
+
 
 public class Tictoe {
     public static void main(String[] args)
@@ -10,13 +10,35 @@ public class Tictoe {
         Scanner scan=new Scanner(System.in);
         char []board=new char[10];
         System.out.println("The TicToe player start fresh");
-        createBoard(scan,board);
+        System.out.println("Player choose x or o and decide if play the game or not.");
+        playerComputer();
     }
     public static void createBoard(Scanner scan, char[] board)
     {
         for(int index=1; index<board.length; index++)
         {
-            board[index]=scan.next().charAt(0);
+            board[index]=' ';
+        }
+    }
+    public static void playerComputer()
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("You have to choose to check you can play or not.");
+        System.out.println("X:Cannot play the game");
+        System.out.println("O:Can play the game");
+        System.out.println("Enter your choice X or O");
+        char ch=scan.next().charAt(0);
+        switch (ch)
+        {
+            case 'X':
+                System.out.println("You Cannot play the game");
+                break;
+            case '0':
+                System.out.println("You Can play the game");
+                break;
+            default:
+                System.out.println("Wrong input");
+                break;
         }
     }
 }
